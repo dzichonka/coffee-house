032a2d9ca968529aec21882b5dfb07297dfe2376
Anna Vasilevich <dzichonka@gmail.com>
2025-10-24 04:33:43 +0200

    feat: add conditional rendering for cards discount

--- src/scripts/modules/cards.ts
+++ src/scripts/modules/cards.ts
@@ -1,6 +1,9 @@
 import { displayModal } from "./modal";
 import { isCategoryType } from "@/types/typeGuards";
 import { fetcher } from "../utils/fetcher";
+import { useAppState } from "../state/appState";
+
+const { isLoggedIn } = useAppState();
 
 const wrapper: HTMLDivElement | null = document.querySelector(".cards");
 const tabs: HTMLDivElement[] = Array.from(
@@ -11,11 +14,6 @@ const refresh: HTMLButtonElement | null = document.querySelector("#refresh");
 let currentCategory: CategoryType = "coffee";
 
 const isMobile = () => window.innerWidth <= 768;
-
-// async function fetchCards(): Promise<ProductsType> {
-//   const response = await fetch("assets/products.json");
-//   return await response.json();
-// }
 
 const { data: res, error } = await fetcher<{
   data: FavoriteProduct[];
@@ -57,10 +55,19 @@ function displayCards(category: CategoryType) {
           <h3>${card.name}</h3>
           <p>${card.description}</p>
         </div>
-      </div>
-        <div class="price-container">
-          <h3>$${card.price}</h3>
+         <div class="price-container">
+${
+  isLoggedIn() && card.discountPrice
+    ? `
+          <h3 class="price">$${card.discountPrice}</h3>
+          <h3 class="price__old">$${card.price}</h3>
+`
+    : `
+          <h3 class="price">$${card.price}</h3>
+`
+}
         </div>
+      </div>
       </div>
     `;
     wrapper.append(cardElement);
